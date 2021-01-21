81cd447134b9ebc87997bfddc11a30aa174c8ba1
faizmotorwala22 <65669797+faizmotorwala22@users.noreply.github.com>
2021-01-21 10:50:14 +0530

    Signup page

---
+++ app/src/main/java/com/example/sign_up/Sign_up_2.java
@@ -0,0 +1,14 @@
+package com.example.sign_up;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Sign_up_2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up_2);
+    }
+}
